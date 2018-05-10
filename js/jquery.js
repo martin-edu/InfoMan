df538aa6b2d981ef09842c3ead5f55376f7851db
Martín <montes.valle1988@gmail.com>
2018-05-10 00:49:19 -0500

    new jquery

--- js/jquery.js
+++ js/jquery.js
@@ -1,15 +1,8 @@
 var tabsGroup = ["COtab", "MCtab", "PPtab", "SStab", "PMtab", "MOEtab", ];
 var sectionsID = [];
-
 var mainObjects = [];
+var sections = [];
 
-function cleanClasses() {
-    for (var index = 0; index < tabsGroup.length; index++) {
-        $("#" + tabsGroup[index]).removeClass("active");
-        $("#" + sectionsID[index]).hide();
-        $("#" + sectionsID[index]).removeClass("notShownInitialClass");
-    }
-}
 
 
 var data;
@@ -21,7 +14,7 @@ $(document).ready(function () {
         success: function (data) {
             var indexAreas;
             var indexVariables;
-            var sections = [];
+            
             var classNotShown = "notShownInitialClass";
             for (indexAreas in data) {
                 sections[indexAreas] = data[indexAreas].area;
@@ -32,24 +25,22 @@ $(document).ready(function () {
                     classNotShown = "notShownInitialClass";
                 }
 
-                $("#sections").append('<div id="' + indexAreas + '" class="row my-4 ' + classNotShown + 
+                $("#sections").append('<div id="' + sections[indexAreas].replace(/ /g, "_") + '" class="row my-4 ' + classNotShown + 
                 '"><h3 class="col-xl-12">' + data[indexAreas].area + '</h3>');
                 sectionsID[indexAreas] = indexAreas;
                 
                 for (indexVariables in data[indexAreas].variables) {
-                    //if(data[indexAreas].variables.length != 0){
-                    $("#" + indexAreas).append('<div class="col-xl-6 my-1">'+
+                    $("#" + sections[indexAreas].replace(/ /g, "_")).append('<div class="col-xl-6 my-1">'+
                         '<div class="input-group"><div class="input-group-prepend">'+
                             '<span class="input-group-text">'
                              + data[indexAreas].variables[indexVariables].field.replace(/_/g, " ") + 
                              '</span>'+
                              '</div>' + 
-                             '<input type="url" class="form-control" value="' + 
-                             data[indexAreas].variables[indexVariables].currentValue + '">'+
+                             '<input type="text" class="form-control" value="' + 
+                             data[indexAreas].variables[indexVariables].currentValue + '" id="'+
+                             sections[indexAreas].replace(/ /g, "_") + indexVariables+'">'+
                              '</div></div>');
-                    //$("#"+indexAreas).append('<input type="url" class="form-control" value="'+data[indexAreas].variables[indexVariables].currentValue+'">');
-                    //console.log(data[indexAreas].variables[indexVariables]);  
-                    //}
+                   
 
                 }
                 console.log(data[indexAreas].variables);
@@ -62,38 +53,45 @@ $(document).ready(function () {
 
     $("#" + tabsGroup[0]).click(function () {
         cleanClasses();
-        $("#" + sectionsID[0]).show();
+        $("#" + sections[0].replace(/ /g, "_")).show();
         $("#" + tabsGroup[0]).addClass("active");
     });
     $("#" + tabsGroup[1]).click(function () {
         cleanClasses();
-        $("#" + sectionsID[1]).show();
+        $("#" + sections[1].replace(/ /g, "_")).show();
         $("#" + tabsGroup[1]).addClass("active");
     });
     $("#" + tabsGroup[2]).click(function () {
         cleanClasses();
-        $("#" + sectionsID[2]).show();
+        $("#" + sections[2].replace(/ /g, "_")).show();
         $("#" + tabsGroup[2]).addClass("active");
     });
     $("#" + tabsGroup[3]).click(function () {
         cleanClasses();
-        $("#" + sectionsID[3]).show();
+        $("#" + sections[3].replace(/ /g, "_")).show();
         $("#" + tabsGroup[3]).addClass("active");
     });
     $("#" + tabsGroup[4]).click(function () {
         cleanClasses();
-        $("#" + sectionsID[4]).show();
+        $("#" + sections[4].replace(/ /g, "_")).show();
         $("#" + tabsGroup[4]).addClass("active");
     });
     $("#" + tabsGroup[5]).click(function () {
         cleanClasses();
-        $("#" + sectionsID[5]).show();
+        $("#" + sections[5].replace(/ /g, "_")).show();
         $("#" + tabsGroup[5]).addClass("active");
     });
 
-
 });
 
-function buildJ(){
+function cleanClasses() {
+    for (var index = 0; index < tabsGroup.length; index++) {
+        $("#" + tabsGroup[index]).removeClass("active");
+        $("#" + sections[index].replace(/ /g, "_")).hide();
+        $("#" + sections[index].replace(/ /g, "_")).removeClass("notShownInitialClass");
+    }
+}
 
+function buildJ(){
+    
 }
---
+++ js/jqueryForm.js
@@ -0,0 +1,74 @@
+var botData;
+$(document).ready(function () {
+    $.ajax({
+        type: "GET",
+        dataType: "json",
+        url: "https://infoman-backend.mybluemix.net/conversation/form",
+        success: function (data) {
+            botData = data;
+            for (var indexAreas in data) {
+                $("#sections").append('<div id="' + indexAreas + '" class="row my-4 ' /*+ classNotShown */ +
+                    '"><h3 class="col-xl-12">' + data[indexAreas].area + '</h3>');
+                for (indexVariables in data[indexAreas].variables) {
+                    $("#" + indexAreas).append('<div class="col-xl-6 my-1">' +
+                        '<div class="input-group"><div class="input-group-prepend">' +
+                        '<span class="input-group-text">' +
+                        data[indexAreas].variables[indexVariables].field.replace(/_/g, " ") +
+                        '</span>' +
+                        '</div>' +
+                        '<input type="text" class="form-control" value="' +
+                        data[indexAreas].variables[indexVariables].currentValue + '" id="' +
+                        data[indexAreas].variables[indexVariables].field.replace(/ /g, "_") + '"></div></div>');
+                }
+
+                $("#sections").append('<button type="button" class="btn btn-primary"' +
+                    'value="' + indexAreas + '">Enviar Cambios</button>');
+            }
+            $(".btn.btn-primary").click(function () {
+                alert($(this).attr('value'));
+                sendJson($(this).attr('value'));
+            });
+
+        }
+    });
+
+
+});
+
+
+
+
+function sendJson(number) {
+    var packageArea = new Object;
+
+    packageArea.area = botData[number].area;
+    packageArea.variables = [];
+    //packageArea.variables = botData[number].variables;
+    //alert(packageArea[number].variables[index].currentValue);
+    for (index in botData[number].variables) {
+        var variablesObject = new Object;
+        //console.log(botData[number].variables[index].field);
+        //console.log(botData[number].variables);
+        variablesObject.field = botData[number].variables[index].field;
+        //console.log($("#" + botData[number].variables[index].field).val());
+        variablesObject.currentValue = $("#" + botData[number].variables[index].field).val();
+        //packageArea[number].variables[index].currentValue = $("#"+botData[number].variables[index].field).val();
+        packageArea.variables.push(variablesObject);
+
+    }
+    console.log(packageArea);
+    //alert(packageArea.variables);
+    $.ajax({
+        type: "PUT",
+        dataType: "application/json",
+        data: JSON.stringify(packageArea),
+        url: "https://infoman-backend.mybluemix.net/conversation/update",
+        success: function(){
+            console.log("success");
+        },
+        error: function(xhr, ajaxOptions, thrownError){
+            console.log(xhr.status);
+            console.log(thrownError);
+        }
+    })
+}
